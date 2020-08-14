c4d21d303645998c487adc1772c1b955f5415ec6
Pe3k-19 <pe3kfpv@gmail.com>
2020-08-14 09:29:26 +0200

    nova componenta Button, Pridanie button1.jsx

--- src/App.js
+++ src/App.js
@@ -4,29 +4,33 @@ import Footer from "./modules/footer/Footer";
 import NavTabs from "./modules/Header/navBar";
 import Header from "./modules/Header/Header";
 import Grid from "@material-ui/core/Grid";
+import Button from "./components/button1";
 
 function App() {
   return (
-    <Grid container direction="column" className="App">
-      <Grid item xs={12}>
-        <Header />
-      </Grid>
-      <Grid container justify="center" className="heading">
-        <Grid item>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Tangerine"
-          />
-          Salón krásy
-        </Grid>
-      </Grid>
-      <Grid container justify="center">
-        <Grid item xs={9}>
-          <NavTabs />
-        </Grid>
-      </Grid>
-      <Footer />
-    </Grid>
+    // <Grid container direction="column" className="App">
+    //   <Grid item xs={12}>
+    //     <Header />
+    //   </Grid>
+    //   <Grid container justify="center" className="heading">
+    //     <Grid item>
+    //       <link
+    //         rel="stylesheet"
+    //         href="https://fonts.googleapis.com/css?family=Tangerine"
+    //       />
+    //       Salón krásy
+    //     </Grid>
+    //   </Grid>
+    //   <Grid container justify="center">
+    //     <Grid item xs={9}>
+    //       <NavTabs />
+    //     </Grid>
+    //   </Grid>
+    //   <Footer />
+    // </Grid>
+    <div>
+      <Button />
+    </div>
   );
 }
 
